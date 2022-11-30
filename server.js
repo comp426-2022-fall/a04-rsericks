2432f7700df30d3a06a3a61c92d348e75e89d07a
root <root@Ryan-Erickson-PC.localdomain>
2022-11-30 13:00:42 -0500

    more server.js

--- server.js
+++ server.js
@@ -26,3 +26,13 @@ app.get('/app/roll/:sides/:dice', (req, res, next) =>{
     res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
     res.end();
 })
+
+app.get('/app/roll/:sides', (req, res, next) => {
+    res.send(roll(parseInt(req.params.sides), 2, 1));
+    res.end();
+})
+
+app.use('/app/roll/:sides/:dice/:rolls', (req, res, next) =>{
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
+    res.end();
+})
