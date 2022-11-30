5d163bf2e2bfd974a5364770aaa789c4d4bf54e6
root <root@Ryan-Erickson-PC.localdomain>
2022-11-30 12:55:14 -0500

    server.js update

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 
-import roll from "./lib/lib/roll.js";
+import {roll} from "./lib/lib/roll.js";
 import minimist from "minimist";
 import express from "express";
 const args = minimist(process.argv.slice(2));
 const app = express();
 const port = args.port ? args.port: 5000;
+
+app.get('/app', (req, res) => {
+    res.send("200 OK");
+    res.end()
+})
+
+app.use((req, res, next) =>{
+    res.status(404).send("404 NOT FOUND");
+    res.end()
+})
