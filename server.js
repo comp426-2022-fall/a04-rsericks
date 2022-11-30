11e62fda98c190aaa0bf56a9a972269c62d602e5
root <root@Ryan-Erickson-PC.localdomain>
2022-11-30 12:57:29 -0500

    another server update

--- server.js
+++ server.js
@@ -9,10 +9,20 @@ const port = args.port ? args.port: 5000;
 
 app.get('/app', (req, res) => {
     res.send("200 OK");
-    res.end()
+    res.end();
 })
 
 app.use((req, res, next) =>{
     res.status(404).send("404 NOT FOUND");
-    res.end()
+    res.end();
+})
+
+app.get('/app/roll',  (req, res, next) =>{
+    res.send(roll(6,2,1));
+    res.end();
+})
+
+app.get('/app/roll/:sides/:dice', (req, res, next) =>{
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
+    res.end();
 })
